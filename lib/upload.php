feab7a478918f5f3ad9e56cb341b399f88a0f721
Tom Trenka <ttrenka@gmail.com>
2010-09-25 16:13:30 +0000

    Set up the uploading utility so that either a url or a file can be provided.  File upload takes precedence.

--- lib/upload.php
+++ lib/upload.php
@@ -13,6 +13,41 @@ function remove_dir($path){
 	rmdir($path);
 }
 
+function process($version, &$log){
+	$s = microtime(1);
+	$dataDir = dirname(__FILE__) . "/../data/";
+	$xslDir = dirname(__FILE__) . "/../xsl/";
+
+	$api_xml = $dataDir . $version . "/api.xml";
+	if(file_exists($api_xml)){
+		$api = new DOMDocument();
+		$api->load($api_xml);
+		$log[] = "API xml file load time: " . (microtime(1) - $s);
+	}
+
+	//	again.
+	if(isset($api)){
+		//	ok, we're good to go.  Let's do this.  LEERRRROOOOYYYYY a-JENKINS!
+		$ss = array("details", "objects", "provides", "resources");
+		foreach($ss as $style){
+			$start = microtime(1);
+			$xsl = new DOMDocument();
+			$xsl->load($xslDir . $style . ".xsl");
+			$p = new XSLTProcessor();
+			$p->importStylesheet($xsl);
+
+			$d = new DOMDocument();
+			$d->loadXML($p->transformToXML($api));
+			$d->save($dataDir . $version . "/" . $style . ".xml");
+			$log[] = $style . " transformation time: " . (microtime(1) - $start);
+		}
+		$log[] = "Total time: " . (microtime(1) - $s);
+		return true;
+	}
+	return false;
+}
+
+//	MAIN
 if(isset($_POST["dir"])){
 	//	generate it.
 	$logger = array();
@@ -21,8 +56,41 @@ if(isset($_POST["dir"])){
 	$xslDir = dirname(__FILE__) . "/../xsl/";
 	$s = microtime(1);
 
+	if(!file_exists($dataDir . $version)){
+		mkdir($dataDir . $version, 0750);
+		mkdir($dataDir . $version . '/cache', 0750);
+	} else {
+		//	clear the cache
+		$f = glob($dataDir . $version . '/cache/*');
+		$files = array_filter($f, 'is_file');
+		array_map('unlink', $files);
+		$dirs = array_filter($f, 'is_dir');
+		array_map('remove_dir', $dirs);	
+	}
+
+	//	check if there was a file uploaded first, and if not default to the URL.
+	if(isset($_FILES["xmlfile"]) && is_uploaded_file($_FILES["xmlfile"]["tmp_name"])){
+		//	save the file.
+		if($_FILES["xmlfile"]["type"] == 'text/xml'){
+			$tmp_name = $_FILES["xmlfile"]["tmp_name"];
+			$name = $_FILES["xmlfile"]["name"];
+			$test = move_uploaded_file($tmp_name, $dataDir . $version . '/api.xml');
+
+			if($test){
+				$logger[] = "Successfully saved the api.xml file.";
+				$success = process($version, $logger);
+			} else {
+				$logger[] = "api.xml file was not saved.";
+				$success = $test;
+			}
+		} else {
+			$logger[] = "Uploaded file is not an XML file.";
+			$success = false;
+		}
+	}
+
 	//	go get the api.xml if we have a URL.
-	if(isset($_POST["api"]) && strlen($_POST["api"])){
+	else if(isset($_POST["api"]) && strlen($_POST["api"])){
 		//	let's cURL it.
 		$request = curl_init();
 		if(defined("CURL_SA_BUNDLE_PATH")){
@@ -43,55 +111,13 @@ if(isset($_POST["dir"])){
 		if($info["http_code"] != 200){
 			$logger[] = "There was an issue getting the api.xml file at '" . $_POST["api"] . "'; returned HTTP " . $info["http_code"] . ".";
 		} else {
-			if(!file_exists($dataDir . $version)){
-				mkdir($dataDir . $version, 0750);
-				mkdir($dataDir . $version . '/cache', 0750);
-			} else {
-				//	clear the cache
-				$f = glob($dataDir . $version . '/cache/*');
-				$files = array_filter($f, 'is_file');
-				array_map('unlink', $files);
-				$dirs = array_filter($f, 'is_dir');
-				array_map('remove_dir', $dirs);	
-			}
-
 			$api = new DOMDocument();
 			$api->loadXML($response);
 			$api->save($dataDir . $version . "/api.xml");
 			$logger[] = "Successfully retrieved the api.xml file.";
-		}
-	}
 
-	//	we didn't pass a URL, or there was an error getting it from somewhere.
-	if(!isset($api)){
-		$api_xml = $dataDir . $version . "/api.xml";
-		if(file_exists($api_xml)){
-			$api = new DOMDocument();
-			$api->load($api_xml);
-			$logger[] = "API xml file load time: " . (microtime(1) - $s);
+			$success = process($version, $logger);
 		}
-	}
-
-	//	again.
-	if(isset($api)){
-		//	ok, we're good to go.  Let's do this.  LEERRRROOOOYYYYY a-JENKINS!
-		$ss = array("details", "objects", "provides", "resources");
-		foreach($ss as $style){
-			$start = microtime(1);
-			$xsl = new DOMDocument();
-			$xsl->load($xslDir . $style . ".xsl");
-			$p = new XSLTProcessor();
-			$p->importStylesheet($xsl);
-
-			$d = new DOMDocument();
-			$d->loadXML($p->transformToXML($api));
-			$d->save($dataDir . $version . "/" . $style . ".xml");
-			$logger[] = $style . " transformation time: " . (microtime(1) - $start);
-		}
-		$logger[] = "Total time: " . (microtime(1) - $s);
-		$is_success = true;
-	} else {
-		$dir_not_found = true;
 	}
 }
 ?>
@@ -103,18 +129,19 @@ if(isset($_POST["dir"])){
 </head>
 <body>
 	<h1>Dojo Toolkit API Documentation Upload Utility</h1>
-<?php if(isset($dir_not_found)){ ?>
-	<div style="font-weight: bold;color:#900;">The directory or original api.xml file for version <?php echo $version; ?> was not found.</div>
-<?php } ?>
-<?php if(isset($is_success)){ ?>
+<?php if(isset($success) && $success){ ?>
 	<div style="font-weight: bold;">Transforms for <?php echo $version; ?> was successful.</div>
-<?php } ?>
+<?php } else if(isset($version) && isset($success)) { ?>
+	<div style="font-weight: bold;color:#900;">The directory or original api.xml file for version <?php echo $version; ?> was not found.</div>
+<?php } ?>	
 <?php 
-	if(isset($logger)){
-		foreach($logger as $log){
-			echo "<div>" . $log . "</div>";
-		}
-	}	
+if(isset($logger)){
+	echo '<div style="margin: 1em; background-color:#efefed;">';
+	foreach($logger as $log){
+		echo "<div>" . $log . "</div>";
+	}
+	echo '</div>';
+}	
 ?>
 	<br/>
 	<p>Enter the DTK version of documentation you wish to process (required), and optionally upload an <strong>api.xml</strong> file.</p>
@@ -128,8 +155,11 @@ if(isset($_POST["dir"])){
 		<div>
 			<label>URL for api.xml: </label>
 			<input type="text" name="api" value="<?php echo (isset($_POST['api'])?$_POST['api']:''); ?>" />
+			<div style="font-weight: bold;">-- OR --</div>
+			<label>Upload api.xml: </label>
+			<input type="file" name="xmlfile" />
 		</div>
-		<div><input type="submit" value="Run the transforms." /></div>
+		<div style="margin-top: 2em;"><input type="submit" value="Run the transforms." /></div>
 		<div>(Note this may take a while, so be patient.)</div>
 	</form>
 </body>
