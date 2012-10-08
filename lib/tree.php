d7b0c99c29c0e9249478b7a2a0b889bacbcce2ec
Bill Keese <bill@dojotoolkit.org>
2012-10-08 18:30:23 +0900

    protect against malicious version string

--- lib/tree.php
+++ lib/tree.php
@@ -6,6 +6,15 @@ include(dirname(__FILE__) . "/../config.php");
 
 $version = isset($_GET["v"]) ? $_GET["v"] : $defVersion;
 
-echo file_get_contents($dataDir . $version . "/tree.json");
+// Get path to tree file, protecting against ".." inserted into $version string by a malicious user
+$path = $dataDir .  preg_replace("/\\.\\.+/", "", $version) . "/tree.json";
+
+if(file_exists($path)){
+    header("Content-Type: application/json");
+    echo file_get_contents($path);
+}else{
+    header("HTTP/1.0 400 Bad Request");
+    echo "Cannot open file $path";
+}
 
 ?>
